b391d13ed5e21736a415a8ec59dd01db08ce88f2
CEB79 <dmsqo79@gmail.com>
2024-03-15 14:57:27 +0900

    journal

--- FInalProject_Health/src/main/java/com/health/dao/JournalDao.java
+++ FInalProject_Health/src/main/java/com/health/dao/JournalDao.java
@@ -3,8 +3,15 @@ package com.health.dao;
 import java.util.List;
 
 import com.health.domain.Journal;
+import com.health.domain.exercise;
 
 public interface JournalDao {
 
+	//운동일지 리스트 출력
 	List<Journal> JournalList(int no);
+
+	//운동일지 상세 출력
+	List<exercise> getExercise(int journalNo);
+	
+	void insertJournal(Journal journal);
 }
--- FInalProject_Health/src/main/java/com/health/domain/Journal.java
+++ FInalProject_Health/src/main/java/com/health/domain/Journal.java
@@ -9,15 +9,19 @@ public class Journal {
 	private String userName;
 	private Timestamp journalDate;
 	private int userNo;
-
+	private int FK_journal_user;
+	
 	public Journal() {}
 
-	public Journal(int journalNo, String journalTitle, String userName, Timestamp journalDate, int userNo) {
+	public Journal(int journalNo, String journalTitle, String userName, Timestamp journalDate, int userNo,
+			int fK_journal_user) {
+		super();
 		this.journalNo = journalNo;
 		this.journalTitle = journalTitle;
 		this.userName = userName;
 		this.journalDate = journalDate;
 		this.userNo = userNo;
+		FK_journal_user = fK_journal_user;
 	}
 
 	public int getJournalNo() {
@@ -60,5 +64,14 @@ public class Journal {
 		this.userNo = userNo;
 	}
 
+	public int getFK_journal_user() {
+		return FK_journal_user;
+	}
+
+	public void setFK_journal_user(int fK_journal_user) {
+		FK_journal_user = fK_journal_user;
+	}
+
+	
 	
 }
